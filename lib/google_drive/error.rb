331ccd80d3d33685e564feaf623b83ae27794488
Mateusz Czerwiński <mateusz.czerwinski@netguru.pl>
2015-12-02 21:54:10 +0100

    remove whitespaces

--- lib/google_drive/error.rb
+++ lib/google_drive/error.rb
@@ -1,12 +1,8 @@
 # Author: Hiroshi Ichikawa <http://gimite.net/>
 # The license of this source is "New BSD Licence"
 
-
 module GoogleDrive
-
     # Raised on errors in this library.
     class Error < RuntimeError
-
     end
-    
 end
