7fb4a6269290400513464ca502da769b6bd1d2b0
Ezequiel <ezedepetris@gmail.com>
2017-05-10 12:54:14 -0300

    change method subcollection by tittle, to search into trash or not

--- lib/google_drive/collection.rb
+++ lib/google_drive/collection.rb
@@ -147,7 +147,7 @@ module GoogleDrive
 
     private
 
-    def files_with_type(type, params = {}, &block, trashed=false)
+    def files_with_type(type, params = {}, trashed=false, &block)
       params = convert_params(params)
       query  = construct_and_query([
         ['? in parents', id],
