3550581bf8317a1e9bdad4aa8c638285109fba36
Sam Bloomquist <sbloomquist@driverreach.com>
2018-07-26 06:32:32 -0400

    Added collection_by_key to GoogleDrive::Session because collection_by_url doesn't seem to work with custom domain gsuite collections.

--- lib/google_drive/session.rb
+++ lib/google_drive/session.rb
@@ -418,6 +418,24 @@ module GoogleDrive
 
     alias folder_by_url collection_by_url
 
+    # Returns GoogleDrive::Collection with given +key+.
+    #
+    # e.g.
+    #   # https://drive.google.com/drive/u/2/folders/1rPPuzAew4tO3ORc88Vz1JscPCcnrX7-J
+    #   session.collection_by_key("1rPPuzAew4tO3ORc88Vz1JscPCcnrX7-J")
+    def collection_by_key(key)
+      file = file_by_id(key)
+      unless file.is_a?(Collection)
+        raise(
+          GoogleDrive::Error,
+          format('The file with the ID is not a folder: %s', key)
+        )
+      end
+      file
+    end
+
+    alias folder_by_key collection_by_key
+
     # Creates a top-level folder with given title. Returns GoogleDrive::Collection
     # object.
     def create_collection(title, file_properties = {})
